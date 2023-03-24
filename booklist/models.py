4490b89a76abc03695ee62658839bf60f6c347c3
phili <philippe.tubery@orange.fr>
2023-03-24 12:00:39 +0100

    My djangoptu/mypj2/booklist app first commit

---
+++ booklist/models.py
@@ -0,0 +1,17 @@
+# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
