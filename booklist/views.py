fdfe4b4f5b265702c68352e93bc32ee1f68aae60
phili <philippe.tubery@orange.fr>
2023-03-28 18:58:15 +0200

    test inverse filtering order

--- booklist/views.py
+++ booklist/views.py
@@ -4,7 +4,7 @@ from .models import Book
 
 def book_list (request):
     ######context = {'title': 'Page d\'accueil PhilTub'}
-    books = Book.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    books = Book.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     ######return render (request, 'booklist/book_list.html', context)
     return render (request, 'booklist/book_list.html', {'books':books})
 
