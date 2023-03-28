e95027144bcb3eae5ff38f48cc9463b72a2ca61e
phili <philippe.tubery@orange.fr>
2023-03-28 17:17:59 +0200

    Added view and template for detailed blog post as well as CSS file

--- booklist/urls.py
+++ booklist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #path('', views.index, name='index'),
     #path('<int:post_id>/', views.detail, name='detail'),
     path('', views.book_list, name='book_list'), #differentiate booklist the app and book_list the view
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
 ]
--- booklist/views.py
+++ booklist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Book
 
@@ -7,3 +7,7 @@ def book_list (request):
     books = Book.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     ######return render (request, 'booklist/book_list.html', context)
     return render (request, 'booklist/book_list.html', {'books':books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'booklist/book_detail.html', {'book': book})
